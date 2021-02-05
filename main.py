651a550279207fe091b2652643761e2a087e5fb8
Osni Pezzini Junior <osni.pezzini@gmail.com>
2021-02-04 23:05:43 -0300

    Configurado rotas utilizando Blueprint

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-from routes import app
+from flask import Flask
 
-if __name__ == '__main__':
+from routes import api
+
+
+def start():
+    app = Flask("ClimaTempo")
+
+    app.register_blueprint(api)
+
     app.run()
 
+
+if __name__ == '__main__':
+    start()
--- routes.py
+++ routes.py
@@ -1,15 +1,15 @@
 from urllib.error import HTTPError
 
-from flask import request, Flask
+from flask import request, Blueprint
 
 from service import ClimaService
 from utils import generate_response
 
-app = Flask("ClimaTempo", )
+api = Blueprint('api', __name__)
 
 
-@app.route("/buscar", methods=["GET"])
-def ola_mundo():
+@api.route("/buscar", methods=["GET"])
+def buscar_clima():
     service = ClimaService()
     args = request.args
     try:
@@ -17,3 +17,8 @@ def ola_mundo():
         return generate_response(200, '', 'tempo', data)
     except HTTPError as he:
         return generate_response(200, 'Dados não encontrados com o termo digitado')
+
+
+@api.route("/historico", methods=["GET"])
+def historico_busca():
+    return generate_response(200, '')
